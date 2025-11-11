4b6cf786e58639d275bfaa1435d255de0c7adbe3
Jayden Merrick <jaydenmerrick07@gmail.com>
2025-11-10 20:49:09 -0600

    Finished checkUser
    Some finishes and polish was added to the checkUser function. It now checks username length, and has more specific character checks to ensure a directory can be made with the email. It also now checks that the email is not used by another account.
    
    Users folder was made to store the user profiles. All profiles have been given a placeholder text file so that they are recognized by github.

--- notebook.c
+++ notebook.c
@@ -41,39 +41,92 @@ int main(){
 		printf("Error launching the active directory.\n");
 		return 1;
 	}
-	char *username_test = malloc(15 * sizeof(char));
-	strcpy(username_test, "user@gmail.com");
+	char *username_test = malloc(40 * sizeof(char));
+	strcpy(username_test, "jmerrick@iastate.edu");
 	char *password_test = malloc(9 * sizeof(char));
 	strcpy(password_test, "PaSSW1!");
-	printf("%d", checkUser(username_test, password_test));	
+	printf("%d", checkUser(username_test, password_test));
+	free(password_test);
+	free(username_test);
 }
 
 /**
 *Verifies the user email and password meet requirements and gives all corresponding error messages
-*Username is an email which must contain an '@' character
+*Username is an email which must contain an '@' character with a '.' character after
+*Username is between 5 and 320 characters
+*Username must not have any odd characters (listed below)
 *Username must not be used by another account
 *Password must have one uppercase letter and one lowercase letter
 *Password must contain a number and special character
 *Password must be between 7 and 28 characters
 *Returns 1 on successful creation or 0 on a failed creation
 */
+//folders cannot contain \/*?"<>|
 int checkUser(char* username, char* password){
 	//Creates a flag that is set to 0 if there is ever an error in email or password
 	int big_flag = 1;
+	int username_length = strlen(username);
+	int password_length = strlen(password);
 	//Username and Password verification
 	//Checks for an email domain
 	if(strchr(username, '@') == NULL){
-		printf("Email must contain an email domain (@domain.com).\n");
+		printf("Email must contain a valid email domain (@domain.com).\n");
 		big_flag = 0;
 	}
+	//ensures there is a . after the @. Runs separate to avoid checking a NULL string
+	else if(strchr(strchr(username, '@'), '.') == NULL){
+		printf("Email must contain a valid email domain (@domain.com).\n");
+		big_flag = 0;
+	}
+	
+	//Checks for valid email length (5-320 characters)
+	if(username_length < 5 || username_length > 320){
+		printf("Email must be between 5 and 320 characters.\n");
+		big_flag = 0;
+	}
+	
+	//Checks if the username contains an unusable character for a directory name (\, /, *, ?, ", <, >, |, :)
+	//Also checks for other odd characters that may cause issues (', ,, ;, (, ), [, ], [space])
+	for(int i = 0; i < username_length; i++){
+		if(strchr("\\/*?\"<>|:',;()[] ", username[i])){
+			printf("Email must not contain unusual characters.\n");
+			big_flag = 0;
+			break;
+		}
+	}
+		
 	//Checks if email is a repeat
-	/*doing this later*/
+	//Opens the Users directory and verifies success
+	DIR *users = opendir("./Users");
+	if(users == NULL){
+		printf("ERROR: Failed to check other users.\n");
+		big_flag = 0;
+	}
+	else{
+		//Reads the subdirectories of Users into a dirent that will include the subdirectory names
+		struct dirent *existing_usernames = readdir(users);
+		while(existing_usernames != NULL){
+			//Checks if the name of the subdirectory matches the username
+			//Subdirectories will be named with the username, so this checks that there is no overlap
+			if(strcmp(existing_usernames->d_name, username) == 0){
+				printf("An account already exists with this email.\n");
+				big_flag = 0;
+				break;
+			}
+			existing_usernames = readdir(users);
+		}
+		closedir(users);
+	}
+	
+			
+	
 	
 	//Checks if password contains an uppercase letter
 	int small_flag = 0;
-	for(int i = 0; i < strlen(password); i++){
+	for(int i = 0; i < password_length; i++){
 		if(isupper((unsigned char)password[i])){
 			small_flag = 1;
+			break;
 		}
 	}
 	if(!small_flag){
@@ -83,9 +136,10 @@ int checkUser(char* username, char* password){
 			
 	//checks if the password contains a lowercase letter
 	small_flag = 0;
-	for(int i = 0; i < strlen(password); i++){
+	for(int i = 0; i < password_length; i++){
 		if(islower((unsigned char)password[i])){
 			small_flag = 1;
+			break;
 		}
 	}
 	if(!small_flag){
@@ -96,8 +150,8 @@ int checkUser(char* username, char* password){
 	//Checks if the password contains a number
 	small_flag = 0;
 	//Iterates through each numeric character (48-57)
-	for(int i = 48; i <= 57; i++){
-		if(strchr(password, (char)i) != NULL){
+	for(int i = 0; i < password_length; i++){
+		if(isdigit((unsigned char)password[i])){
 			small_flag = 1;
 			break;
 		}
@@ -142,7 +196,7 @@ int checkUser(char* username, char* password){
 	}
 	
 	//Checks password length
-	if(strlen(password) < 7 || strlen(password) > 28){
+	if(password_length < 7 || password_length > 28){
 		printf("Password must be between 7 and 28 characters.\n");
 		big_flag = 0;
 	}
